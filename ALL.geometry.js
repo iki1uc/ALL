43cb6fdfe85931e795037cec9f6d5dc634d7ad51
iki1uc <entertrainer@web.de>
2026-07-23 09:05:14 +0200

    Update ALL.geometry.js

--- ALL.geometry.js
+++ ALL.geometry.js
@@ -3,21 +3,22 @@ class ALL_GEOMETRY {
     build(respo){
         return respo.map((name, i)=>{
 
-            const score = (name.length * 7 + i * 3) % 100;
-            const deg   = (name.length * 13 + i * 5) % 360;
+            const score = ((i + name.length) * 7) % 100;
+            const deg   = ((i + name.length) * 13) % 360;
 
             const color =
-                score > 70 ? "green" :
-                score > 40 ? "yellow" :
+                score >= 70 ? "green" :
+                score >= 40 ? "yellow" :
                 "red";
 
             return {
+                id: i,
                 name,
                 score,
                 deg,
                 color,
 
-                // 3D-Position
+                // 3D Position
                 x: Math.sin(deg * Math.PI/180) * 50,
                 y: Math.cos(deg * Math.PI/180) * 50,
                 z: (score - 50),
