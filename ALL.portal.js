9f959db862b487f71ef8764d9f3f508f04cc3fa4
iki1uc <entertrainer@web.de>
2026-07-23 09:00:36 +0200

    Create ALL.portal.js

---
+++ ALL.portal.js
@@ -0,0 +1,30 @@
+class ALL_PORTAL {
+
+    jump(list){
+        return list.map((cell, i)=>{
+
+            const portal =
+                i % 4 === 0 ? "A" :
+                i % 4 === 1 ? "B" :
+                i % 4 === 2 ? "C" :
+                "D";
+
+            let warp = i;
+
+            if(portal === "A") warp = i + 20;
+            if(portal === "B") warp = i - 15;
+            if(portal === "C") warp = (i * GAM.gamma()) % 27;
+            if(portal === "D") warp = (i % 9) * 3 + (i % 3);
+
+            warp = Math.abs(warp % 27);
+
+            return {
+                ...cell,
+                portal,
+                warp
+            };
+        });
+    }
+}
+
+window.ALL_PORTAL = new ALL_PORTAL();
